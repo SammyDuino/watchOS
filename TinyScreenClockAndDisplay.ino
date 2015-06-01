d3eda458aa3e7337d3bbb9a3de9abc8561527be1
PacksGamingHD <craftyswans@gmail.com>
2015-05-31 17:27:25 -0700

    This commit is a very big release. You can see the notes in the Releases section

--- TinyScreenClockAndDisplay.ino
+++ TinyScreenClockAndDisplay.ino
@@ -23,54 +23,48 @@ int int_number = 1;
 
 DS1339 RTC = DS1339();
 
-unsigned long lastReceivedTime=0;
-
-unsigned long lastDisplayUpdate=0;
-unsigned long displayUpdateInterval=1000;
-
-
-unsigned long sleepTimer=0;
-unsigned long sleepTimeout=5000;
 int displayState=0;
-int buttonReleased=1;
+int buttonReleased=0;
+int countdown = 0;
 
-char notificationLine1[20]="Release 2.0.0";
-char notificationLine2[20]="";
+char notificationLine1[20]="Release 2.1.1";
 
 void setup(void) { // setup
   Wire.begin();
   display.begin();
   display.setFlip(1);
-  display.setBrightness(3);
+  display.setBrightness(4);
   RTC.start();
+  displayBattery();
   display.fontColor(GREEN,BLACK);
   display.setFont(liberationSans_16ptFontInfo);
   display.setCursor(0,10);
-  display.print("Hello");
+  display.print("Welcome");
+  display.setCursor(0,40);
+  display.print("Booting up");
+  Serial.print("Initializing");
   delay(2000);
   display.clearWindow(0,0,0,0);
 }
 
 void loop() {
   byte buttons = display.getButtons();
-  if(buttonReleased && buttons){
-  	if(displayState){
-      notificationLine1[0]='\0';
-      memcpy(notificationLine2,"  No notifications.",strlen("  No notifications."));
-      displayNotifications();
-  	}
+  if(buttons != 0)
+  {
+    displayState = 1;
     display.on();
-    buttonReleased=0;
-  }
-  if(!buttonReleased && !(buttons&0x0F)){
-    buttonReleased=1;
-  }
-  if(millis() > sleepTimer+sleepTimeout){
-    if(displayState){
-      displayState=0;
-      display.off();
+    countdown = 90;
+  	}
+    if(buttons == 0){
+      countdown=countdown-1;
+      Serial.println(countdown);
     }
-  }
+  if (countdown<=0){
+  displayState=0;
+  display.off();
+}
+  displayNotifications();
+  displayBattery();
   unsigned char second;
   unsigned char minute;
   unsigned char hour;
@@ -85,8 +79,7 @@ void loop() {
         day = RTC.getDays();
         month = RTC.getMonths();        
         year = RTC.getYears();
-        
-  display.setFont(liberationSans_10ptFontInfo);    
+  display.setFont(liberationSans_10ptFontInfo);  
   // Date      
   display.setCursor(0,10);
   display.fontColor(BLUE,BLACK);
@@ -100,7 +93,12 @@ void loop() {
   // Time
   display.setCursor(0,30);
   display.fontColor(GREEN,BLACK);
-  display.print(hour);
+  int hours = RTC.getHours();
+  if (hours > 12)
+{
+  hours = hours - 12;
+}
+display.print(hours);
   display.print(":");
   
   if (RTC.getMinutes() < 10) // if minutes under 10
@@ -129,8 +127,6 @@ void loop() {
    {
      display.fontColor(RED,BLACK);
      display.print(second);
-     displayBattery();
-     displayNotifications();
    }
 }
 void displayNotifications(){
