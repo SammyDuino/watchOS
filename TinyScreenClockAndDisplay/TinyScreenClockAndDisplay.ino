29652d8f2c702fdab1adf7edc463a10a972f0ad2
PacksGamingHD <craftyswans@gmail.com>
2015-12-03 21:19:53 -0800

    rewrite

--- TinyScreenClockAndDisplay/TinyScreenClockAndDisplay.ino
+++ TinyScreenClockAndDisplay/TinyScreenClockAndDisplay.ino
@@ -1,4 +1,4 @@
-#define  BLACK           0x00
+ #define  BLACK           0x00
 #define BLUE            0xE0
 #define RED             0x03
 #define GREEN           0x1C
@@ -31,17 +31,36 @@ int countdown = 0;
 int messages = 0;
 // Defines integers being used
 
-void setup(void) 
-{
-  startAllProcesses();
-  startup();
-  display.setCursor(0,30);
-}
 
-void loop() 
+void setup()
 {
+  start_coreservices();
+  start_effects();
+  display_battery();
+}
+void loop()
+{
+  unsigned char second;
+  unsigned char minute;
+  unsigned char hour;
+  unsigned char month;
+  unsigned char day;
+  unsigned int year;
+  unsigned char hr;
+  unsigned int weekday;
+  char* dayOfWeekName[7];
+  display.fontColor(BLACK,BLACK);
+  RTC.readTime();
+  second = RTC.getSeconds();
+  minute = RTC.getMinutes();
+  hour = RTC.getHours();        
+  day = RTC.getDays();
+  month = RTC.getMonths();        
+  year = RTC.getYears();
+  weekday = RTC.getDayOfWeek();
+  hr = RTC.getHours();
+  Serial.println(RTC.getDayOfWeek());
   byte buttons = display.getButtons();
-  
   if (buttons != 0)
   {
     displayState = 1;
@@ -59,95 +78,131 @@ void loop()
   displayState = 0;
   display.off();
   }
-
   display.setFont(liberationSans_10ptFontInfo);
   display.fontColor(WHITE,BLACK);
   display.setCursor(0,40);
-if (messages == 1)
-{
-    display.fontColor(YELLOW,BLACK);
-    display.print("watchOS beta 1");
-}
-    if (messages == 10)
-    {
-      display.clearWindow(0,40,0,40);
-      display.fontColor(BLUE,BLACK);
-      display.print("hello world");
-    }
-    if (messages == 20)
-    {
-                  display.clearWindow(0,40,0,40);
-        display.fontColor(GREEN,BLACK);
-      display.print("Sammy Watch");
-    }
-    if (messages == 30)
-    {
-                  display.clearWindow(0,40,0,40);
-        display.fontColor(RED,BLACK);
-      display.print("hello world");
-    }
-    if (buttons == 2)
-    {
-      messages=0;
-      display.off();
-      display.clearWindow(0,40,0,40);
-      display.on();
-    }
-  displayBattery();
-  unsigned char second;
-  unsigned char minute;
-  unsigned char hour;
-  unsigned char month;
-  unsigned char day;
-  unsigned int year;
-  unsigned char hr;
-  unsigned int weakday;
-  char* dayOfWeekName[7];
-  display.fontColor(BLACK,BLACK);
-  RTC.readTime();
-        second = RTC.getSeconds();
-        minute = RTC.getMinutes();
-        hour = RTC.getHours();        
-        day = RTC.getDays();
-        month = RTC.getMonths();        
-        year = RTC.getYears();
-        weakday = RTC.getDayOfWeek();
-        hr = RTC.getHours();
-// weakday
-  if (messages > 40)
+  display.setCursor(0,30);
+  display.fontColor(WHITE,BLACK);
+  display.setFont(liberationSans_16ptFontInfo);
+  display.setFont(liberationSans_22ptFontInfo);
+  display.setCursor(0,16);
+  if (hour > 12) {
+  hour = hour - 12;
+  }
+  display.print(hour);
+  display.print(":");
+  if (minute < 10) {
+    display.print("0");
+  }
+  display.print(minute);
+  display.setFont(liberationSans_12ptFontInfo);
+  if (hr >= 12)
+  {
+  display.print("PM");
+  } else {
+  display.print("AM");
+  }
   {
   display.setFont(liberationSans_16ptFontInfo);
   display.fontColor(WHITE,BLACK);
   display.setCursor(0,40);
- switch (weakday) {
-  case 0:
-  display.print("Monday");
-  break;
-  case 1:
-  display.print("Tuesday");
-  break;
-  case 2:
-  display.print("Wednsday");
-  break;
-  case 3:
-  display.print("Thursday");
-  break;
-  case 4:
-  display.print("Friday");
-  break;
-  case 5:
-  display.print("Saturday");
-  break;
-  case 6:
-  display.print("Sunday");
-  break;
   }
-  }
-// Date      
+// Date  
   display.setFont(liberationSans_10ptFontInfo);
   display.fontColor(WHITE,BLACK);
   display.setCursor(1,1);
   // Months
+  rtc_months();
+  display.print(" ");
+  switch (day) {
+  case 1:
+  display.print("1st");
+  break;
+  case 2:
+  display.print("2nd");
+  break;
+  case 3:
+  display.print("3rd");
+  break;
+  case 21:
+  display.print("21st");
+  break;
+  case 22:
+  display.print("22nd");
+  break;
+  case 23:
+  display.print("23rd");
+  break;
+  case 31:
+  display.print("31st");
+  break;
+  }
+  if ((day >=4) && (day<=20))
+  {
+  display.print(day);
+  display.print("th");
+  }
+  if ((day >= 24) && (day <= 30))
+  {
+  display.print(day);
+  display.print("th");
+  }
+}
+void start_coreservices()
+{
+  Wire.begin();
+  display.begin();
+  display.setFlip(1);
+  display.setBrightness(5);
+  Serial.begin(9600);
+  RTC.start();
+}
+void display_battery()
+{
+  const long InternalReferenceVoltage = 1100L;
+  ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
+  delay(10);
+  ADCSRA |= _BV( ADSC );
+  while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
+  int result = (((InternalReferenceVoltage * 1024L) / ADC) + 5L) / 10L;
+  result=constrain(result-300,0,120);
+  int x=72;
+  int y=2;
+  int height=8;
+  int length=20;
+  int amtActive=(result*length)/110;
+  int red,green,blue;
+  for(int i=0;i<length;i++){
+  if (i<amtActive) 
+  {
+    red=63-((63/length)*i);
+    green=((63/length)*i);
+    blue=0;
+    } 
+    else {
+      red=32;
+      green=32;
+      blue=32;
+    }
+    display.drawLine(x+i,y,x+i,y+height,red,green,blue);
+  }
+}
+void start_effects()
+{
+  display.fontColor(GREEN,BLACK);
+  display.setFont(liberationSans_16ptFontInfo);
+  display.setCursor(0,10);
+  display.print("Welcome");
+  delay(2000);
+  displayState = 1;
+  display.on();
+  countdown = 90;
+  display.clearWindow(0,0,0,0);
+}
+void rtc_months()
+{
+  unsigned char month;
+  month = RTC.getMonths();  
   switch (month) {
   case 1:
   display.print("Jan");
@@ -186,8 +241,11 @@ if (messages == 1)
   display.print("Dec");
   break;
   }
-  display.print(" ");
-  // Day
+}
+void rtc_day()
+{  
+  unsigned char day;
+  day = RTC.getDays();
   switch (day) {
   case 1:
   display.print("1st");
@@ -221,75 +279,4 @@ if (messages == 1)
   display.print(day);
   display.print("th");
   }
-// Time
-  display.setFont(liberationSans_22ptFontInfo);
-  display.setCursor(0,16);
-  if (hour > 12) {
-  hour = hour - 12;
-  }
-  display.print(hour);
-  display.print(":");
-  if (minute < 10) {
-    display.print("0");
-  }
-  display.print(minute);
-
-if (hr >= 12)
-{
-  display.print("PM");
-}
-else {
-  display.print("AM");
-}
-}
-void displayBattery()
-{
-  const long InternalReferenceVoltage = 1100L;
-  ADMUX = (0<<REFS1) | (1<<REFS0) | (0<<ADLAR) | (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (0<<MUX0);
-  delay(10);
-  ADCSRA |= _BV( ADSC );
-  while( ( (ADCSRA & (1<<ADSC)) != 0 ) );
-  int result = (((InternalReferenceVoltage * 1024L) / ADC) + 5L) / 10L;
-  result=constrain(result-300,0,120);
-  int x=72;
-  int y=2;
-  int height=8;
-  int length=20;
-  int amtActive=(result*length)/110;
-  int red,green,blue;
-  for(int i=0;i<length;i++){
-  if (i<amtActive) 
-  {
-    red=63-((63/length)*i);
-    green=((63/length)*i);
-    blue=0;
-    } 
-    else {
-      red=32;
-      green=32;
-      blue=32;
-    }
-    display.drawLine(x+i,y,x+i,y+height,red,green,blue);
-  }
-}
-void startup()
-{
-  display.fontColor(GREEN,BLACK);
-  display.setFont(liberationSans_16ptFontInfo);
-  display.setCursor(0,10);
-  display.print("Welcome");
-  delay(2000);
-  displayState = 1;
-  display.on();
-  countdown = 90;
-  display.clearWindow(0,0,0,0);
-}
-void startAllProcesses()
-{
-  Wire.begin();
-  display.begin();
-  display.setFlip(1);
-  display.setBrightness(5);
-  Serial.begin(9600);
-  RTC.start();
 }
